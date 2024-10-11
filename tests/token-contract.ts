32c16052de38ec079c7ab9efdc071551495a64ca
simbahebinbo <lansheng228@163.com>
2024-10-11 13:54:13 +0800

    format code

--- tests/token-contract.ts
+++ tests/token-contract.ts
@@ -21,7 +21,7 @@ describe("token-contract", () => {
 
     it("Mint a token", async () => {
         // Get anchor's wallet's public key
-        const key = anchor.AnchorProvider.env().wallet.publicKey;
+        const myWallet = anchor.AnchorProvider.env().wallet.publicKey;
         // Get the amount of SOL needed to pay rent for our Token Mint
         const lamports: number = await program.provider.connection.getMinimumBalanceForRentExemption(
             MINT_SIZE
@@ -30,14 +30,14 @@ describe("token-contract", () => {
         // Get the ATA for a token and the account that we want to own the ATA (but it might not existing on the SOL network yet)
         associatedTokenAccount = await getAssociatedTokenAddress(
             mintKey.publicKey,
-            key
+            myWallet
         );
 
         // Fires a list of instructions
         const mint_tx = new anchor.web3.Transaction().add(
             // Use anchor to create an account from the mint key that we created
             anchor.web3.SystemProgram.createAccount({
-                fromPubkey: key,
+                fromPubkey: myWallet,
                 newAccountPubkey: mintKey.publicKey,
                 space: MINT_SIZE,
                 programId: TOKEN_PROGRAM_ID,
@@ -45,11 +45,11 @@ describe("token-contract", () => {
             }),
             // Fire a transaction to create our mint account that is controlled by our anchor wallet
             createInitializeMintInstruction(
-                mintKey.publicKey, 0, key, key
+                mintKey.publicKey, 0, myWallet, myWallet
             ),
             // Create the ATA account that is associated with our mint on our anchor wallet
             createAssociatedTokenAccountInstruction(
-                key, associatedTokenAccount, key, mintKey.publicKey
+                myWallet, associatedTokenAccount, myWallet, mintKey.publicKey
             )
         );
 
@@ -62,14 +62,14 @@ describe("token-contract", () => {
 
         console.log("Account: ", res);
         console.log("Mint key: ", mintKey.publicKey.toString());
-        console.log("User: ", key.toString());
+        console.log("User: ", myWallet.toString());
 
         // Executes our code to mint our token into our specified ATA
         await program.methods.mintToken().accounts({
             mint: mintKey.publicKey,
             tokenProgram: TOKEN_PROGRAM_ID,
             tokenAccount: associatedTokenAccount,
-            authority: key,
+            authority: myWallet,
         }).rpc();
 
         // Get minted token amount on the ATA for our anchor wallet
